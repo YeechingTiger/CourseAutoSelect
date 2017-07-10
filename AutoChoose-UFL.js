5e737a2749e0132f64f31c916b7508653d76851f
JasonHo <hexing_h@hotmail.com>
2017-07-10 15:03:40 +0800

    Update.

--- AutoChoose-UFL.js
+++ AutoChoose-UFL.js
@@ -7,8 +7,7 @@
 //in which all course rows are showed and you should not open any of the course rows.
 //Then open Chrome devtools and paste the code into the console.
 
-//When the script add any of the course for you, it will stop and redirect to your course schedule.
-
+//When the script adds any of the course for you, it will stop and redirect to your course schedule.
 
 
 //Code:
@@ -20,38 +19,36 @@ var courseArr = [1, 2, 3];
 
 var count = 0;
 function btnClick() {
-	document.getElementsByClassName("modal-confirm-btn")[0].click();
+    document.getElementsByClassName("modal-confirm-btn")[0].click();
 }
 function chooseCourse() {
-	if (count === courseArr.length - 1)
-		count = 0;
-	var location = courseArr[count];
-	document.getElementsByClassName("course-code")[location].click();
-	if (document.getElementsByClassName("section-add-section")[0]!==undefined) 
-	{
-		document.getElementsByClassName("section-add-section")[0].click();
-		setTimeout(btnClick, 3000);
-		setTimeout(btnClick, 4000);
-		setTimeout(btnClick, 4500);
-	}
-	else
-	{	
-			setTimeout(function(){
-				document.getElementsByClassName("md-ink-ripple")[7].click();
-			}, 1000
-			)
-			
-			setTimeout(function(){
-				document.getElementsByClassName("filter-sidebar-search-button")[0].click();
-			}, 2000
-			)
+    if (count === courseArr.length - 1)
+        count = 0;
+    var location = courseArr[count];
+    document.getElementsByClassName("course-code")[location].click();
+    if (document.getElementsByClassName("section-add-section")[0] !== undefined) {
+        document.getElementsByClassName("section-add-section")[0].click();
+        setTimeout(btnClick, 3000);
+        setTimeout(btnClick, 4000);
+        setTimeout(btnClick, 4500);
+    }
+    else {
+        setTimeout(function () {
+                document.getElementsByClassName("md-ink-ripple")[7].click();
+            }, 1000
+        );
 
-			setTimeout(function(){
-				console.log("Don't find any open seat. Refreshing again...");
-			}, 3000
-			)
-	}
-	count++;
+        setTimeout(function () {
+                document.getElementsByClassName("filter-sidebar-search-button")[0].click();
+            }, 2000
+        );
+
+        setTimeout(function () {
+                console.log("Don't find any open seat. Refreshing again...");
+            }, 3000
+        )
+    }
+    count++;
 }
 
 setInterval(chooseCourse, 10000);
