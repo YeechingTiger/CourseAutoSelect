f9667e3581e7d83109b56fa331993a3d3f1b3146
JasonHo <hexing_h@hotmail.com>
2017-08-15 12:46:15 -0400

    Version 1.1
    Add Chinese instruction.

--- AutoChoose-UFL.js
+++ AutoChoose-UFL.js
@@ -1,6 +1,6 @@
 //@COPYRIGHT: Jason Ho
 //Created Time: Monday, July 10, 
-//Version: beta
+//Version: Version 1.1
 
 //Usage: 
 //Use Google Chrome and enter course select page. The course select page is the page
@@ -14,6 +14,13 @@
 // The number is the position of the course. The first course is 0. 
 // And you need check the 'Sections with no Open seats' in the course 
 // filter to make sure the course position will not change.
+
+// 使用指南
+// 使用谷歌浏览器在One.ufl.edu进入到选课界面，记得要在filter里勾选'Sections with no Open seats'选项。
+// 找到想选的课在第几个，把courseArr数组里的数字改成你想选的课的位置，从0开始数起。
+// 进入console,复制修改后的代码然后按回车，便可以自动刷课。
+// 测试一天刷近4000次会被踢下,再次登陆即可。
+
 var courseArr = [1, 2, 3];
 
 
